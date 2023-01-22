20785d36cb0210d9f56ef484f461b161b61441c5
fl0rchus <quinterosflorencia0@gmail.com>
2023-01-22 13:52:58 -0300

    server setup, loginHandler created

---
+++ server/controllers/auth.controller.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+
+//Función para manejar la lógica del login
+export const loginHandler = (req: Request, res: Response) => {
+  /*
+        1) Procesar el req.body
+        2) Validar los datos (email, password) - express-validator, joi, zod
+        3) Almacenar los valores en la db 
+        4) Generar un token <-- 
+
+    */
+
+  const token = jwt.sign(
+    {
+      test: "test",
+    },
+    "secret",
+    {
+      expiresIn: 60 * 60 * 24,
+    }
+  );
+
+  return res.json({ token });
+};
